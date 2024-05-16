1e3b79989b3ae8bcd8707e1e586c05eca013e1cb
Gaurav Kale <120704987+gauravkale1@users.noreply.github.com>
2024-05-16 12:41:28 +0530

    Updated Login Component, App Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/authentication/login/login.component';
+import { FooterComponent } from './pages/shared-components/footer/footer.component';
+import { HeaderComponent } from './pages/shared-components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
