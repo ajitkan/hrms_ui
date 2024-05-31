9833bb4f5f720aa43fa2dd5ea71060e24dad8942
ajitkan <ajit.sonvane@kaninfos.com>
2024-05-31 18:01:37 +0530

    Ajit_Code-31-05

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,15 @@ export class AppComponent {
   ngOnInit(){
     // alert("login Success");
     // this.IsLogin = true;
+    if(localStorage.getItem('LoggedIn') !== null){
+      this.IsLogin = Boolean(localStorage.getItem('LoggedIn'))
+    }
+
   }
   IsLoggedin(status:any){
     if(status)
       this.IsLogin = true;
+      localStorage.setItem('LoggedIn',this.IsLogin.toString());
+
   }
 }
---
+++ src/app/pages/time-and-attendance/import-attendances/import-attendances.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-import-attendances',
+  templateUrl: './import-attendances.component.html',
+  styleUrls: ['./import-attendances.component.css']
+})
+export class ImportAttendancesComponent {
+
+}
