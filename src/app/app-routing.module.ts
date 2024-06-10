9d4db73bc515d3b0fbf18889a94f5d7ff4fb6f6d
ajitkan <ajit.sonvane@kaninfos.com>
2024-06-10 15:21:24 +0530

    ajit_chang

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -70,7 +70,7 @@ const routes: Routes = [
 
  {
   path: 'company-master', component: CompanyMasterComponent
- }
+ },
 
   // { 
   //   path: 'emp-contact-details', component: ContactDetailsComponent 
